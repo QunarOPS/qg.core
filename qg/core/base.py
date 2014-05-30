73ded2175c90e525e4264291ad17a1b5e6e5d354
Jianing Yang <jianingy.yang@gmail.com>
2014-05-30 19:27:23 +0800

    move in openstack commons

---
+++ qg/core/base.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright 2013, Qunar OPSDEV
+#
+#    Licensed under the Apache License, Version 2.0 (the "License");
+#    you may not use this file except in compliance with the License.
+#    You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS,
+#    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#    See the License for the specific language governing permissions and
+#    limitations under the License.
+#
+# Author: zhen.pei <zhen.pei@qunar.com>
+# Author: Jianing Yang <jianing.yang@qunar.com>
+#
+
+from abc import ABCMeta, abstractmethod
+
+
+class QApplicationBase(object):
+
+    __metaclass__ = ABCMeta
+
+    @abstractmethod
+    def init_app(self):
+        """在程序启动阶段初始化一个应用程序"""
+        pass
+
+    @abstractmethod
+    def configure(self):
+        """通过 oslo.config 配置一个应用程序"""
+        pass
+
+    @abstractmethod
+    def run(self):
+        """运行应用程序"""
+        pass
+
+    @abstractmethod
+    def shutdown(self):
+        """完成程序退出时的清理工作"""
+        pass
